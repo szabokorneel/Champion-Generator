ce0f915d1e03cb128d17944e07a2f7d0200e1fb1
szabokorneel <kornelszabo536@gmail.com>
2022-11-15 09:29:19 +0100

    menü kész

--- functions.py
+++ functions.py
@@ -1,4 +1,35 @@
-from main import *
+from data import csapatok
+from os import system
+filename = 'csapatok.csv'   
 
-def yourTeam():
+def menu():
+    system('cls')
+    print('0 - Csapatnév megadása')
+    print('1 - Csapat mentése')
+    print('2 - Csapatok Listázás')
+    print('3 - Meccs lejátszása')
+    print('4 - Kilépés')
+    return input('Kérem válasszon: ')
+
+
+def teams():
+    file = open(filename, 'r', encoding = 'utf-8' )
+    global cimsor
+    cimsor = file.readline() 
+    for row in file:
+        splitted = row.strip().split(';')
+        csapatok[splitted[0]] = int(splitted[1])
+
+def addYourTeam():
+    pass
+
+
+def saveTeam():
+    pass
+
+
+def teamsList():
+    pass
+
+def playMatch():
     pass
--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from functions import *
+from functions import teams, menu, addYourTeam, saveTeam, teamsList, playMatch
 from os import system
 
 system('cls')
 print (f'Üdvözöljük a Champion Generator-ben!')
 
 print(f'A játék során 8 csapatból, verejtékes mérkőzések eredményeként csak 1 csapat maradhat: A BAJNOK CSAPAT.')
+
+choice = ''
+while choice != '4':
+    choice = menu()
+    if choice == '0':
+        addYourTeam()
+    elif choice == '1':
+        saveTeam()
+    elif choice == '2':
+        teamsList()
+    elif choice == '3':
+        playMatch()
+    
